b521e540ba4451be61cd266352f32bf63a711166
lokeshe21 <66429225+lokeshe21@users.noreply.github.com>
2021-10-05 00:39:35 +0530

    Add files via upload

---
+++ India1.java
@@ -0,0 +1,17 @@
+public abstract class India1{
+	
+	static String capital = "New Delhi";
+	
+public abstract void speakLanguage();
+public abstract void eat();
+public abstract void dress();
+public India1(){
+		
+	}
+public India1(String primeMinister)
+{
+System.out.println("our Prime Minister is" + primeMinister);
+}		
+		
+		
+}
